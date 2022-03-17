465700dfaab857b3c3adb963ee80bbb34d5f2789
MinhoJJang <ahchjang@naver.com>
2022-03-17 16:59:34 +0900

    0317

--- 07_Queue/QueueMain.c
+++ 07_Queue/QueueMain.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h> // malloc, rand, atoi .. 동적메모리, 난수, 문자열 변환
 #include <string.h> // strcpy .. 문자열 함수.  memcpy.. 메모리블럭 함수
-#include <conio.h>  // _getch, putch .. MS 기반 C비표준 함수
-#include <time.h>   // clock .. 시간 날짜 함수
-#pragma warning(disable:4996)  // scanf() 등 전통 C함수중 버퍼문제.
-#pragma warning(disable:4477)  // unsigned <--> signed 관련 warning
+//#include <conio.h>  // _getch, putch .. MS 기반 C비표준 함수
+#include <time.h>				// clock .. 시간 날짜 함수
+#pragma warning(disable : 4996) // scanf() 등 전통 C함수중 버퍼문제.
+#pragma warning(disable : 4477) // unsigned <--> signed 관련 warning
 
-#define SUCCESS	1
-#define FAIL	0
+#define SUCCESS 1
+#define FAIL 0
 
 #define YES 1
 #define NO 0
@@ -23,29 +23,27 @@ int main()
 	queue_init(&q);
 
 	// enqueue
-	queue_enq(&q, 1);   // front <-- [1] <-- back
-	queue_enq(&q, 2);   // front <-- [1 2] <-- back
-	queue_enq(&q, 3);   // front <-- [1 2 3] <-- back
-	queue_enq(&q, 4);   // front <-- [1 2 3 4] <-- back
-	queue_enq(&q, 5);   // front <-- [1 2 3 4 5] <-- back
+	queue_enq(&q, 1); // front <-- [1] <-- back
+	queue_enq(&q, 2); // front <-- [1 2] <-- back
+	queue_enq(&q, 3); // front <-- [1 2 3] <-- back
+	queue_enq(&q, 4); // front <-- [1 2 3 4] <-- back
+	queue_enq(&q, 5); // front <-- [1 2 3 4 5] <-- back
 
 	queue_deq(&q, &data);
-	printf("deq --> %d\n", data);  // 1
+	printf("deq --> %d\n", data); // 1
 	queue_deq(&q, &data);
-	printf("deq --> %d\n", data);  // 2
+	printf("deq --> %d\n", data); // 2
 
 	while (queue_is_empty(&q) != YES)
-		// queue_is_empty(&q) != YES 은 
-		// !queue_is_empty(&q) 와 같은 의미이다. 
+	// queue_is_empty(&q) != YES 은
+	// !queue_is_empty(&q) 와 같은 의미이다.
 	{
 		queue_deq(&q, &data);
-		printf("%d ", data);    // 3 4 5
+		printf("%d ", data); // 3 4 5
 	}
 	printf("\n");
 
-
-
-	queue_destroy(&q);  // 큐 제거
+	queue_destroy(&q); // 큐 제거
 
 	printf("\n아무키나 입력하면 프로그램 종료됩니다\n");
 	_getch();
